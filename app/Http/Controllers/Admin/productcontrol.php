290e36542901619676e8b926a38e066cf5aa3f5f
Kesha005 <backendtm005@gmail.com>
2022-11-03 15:03:42 +0500

    productcontrolmerge

--- app/Http/Controllers/Admin/productcontrol.php
+++ app/Http/Controllers/Admin/productcontrol.php
@@ -15,15 +15,10 @@ class productcontrol extends Controller
 {
     public function index()
     {
-<<<<<<< HEAD
-        $products=products::with('category','mark')->paginate(20);
-        return view('admin.products.index',compact('products'));
-=======
         $marks=marks::all();
         $categories=category::all();
         $products=products::with('category','mark')->paginate(20);
         return view('admin.products.index',compact('products','marks','categories'));
->>>>>>> 18d6ff46b809260d7f5e6fb5d1dccb77119ec937
     }
 
     public function create()
@@ -33,25 +28,6 @@ class productcontrol extends Controller
 
     public function store(productrequest $request)
     {
-<<<<<<< HEAD
-        $images=[];
-        if($request->image)
-        {
-            foreach($request->image as $img)
-            {
-                $img_name=time().rand(1,99).'.'.$img->extension();
-                $img->storeAs('public/products',$img_name);
-                $images[]=$img_name;
-            }
-            $validated['image']=$images;
-        }
-        $this->saveproduct($validated);
-        return '';
-    }
-
-    public function saveproduct($validated)
-    {
-=======
         $validated=$request->all();
         $validated['image']=$request->image->store('products','public');
         $this->saveproduct($validated);
@@ -65,7 +41,6 @@ class productcontrol extends Controller
 
     public function saveproduct($validated)
     {
->>>>>>> 18d6ff46b809260d7f5e6fb5d1dccb77119ec937
         products::create($validated);
     }
 
