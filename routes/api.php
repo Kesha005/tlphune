64fbf4c7a2a0e4105a6bbc130c49ddc417588e25
Kesha005 <backendtm005@gmail.com>
2022-11-18 14:55:31 +0500

    ds

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\API\addpostcontrol;
 use App\Http\Controllers\API\logincontrol;
 use App\Http\Controllers\API\basecontrol;
 use App\Http\Controllers\API\shopcontrol;
+use Faker\Provider\Base;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -36,6 +37,8 @@ Route::get('/category/{id}',[basecontrol::class,'category']);
 Route::get('/category/{category_id}/marks/{mark_id}',[basecontrol::class,'filter']);
 
 Route::get('/category/{category_id}',[basecontrol::class,'allmark']);
+
+Route::get('/event/{event_id}',[basecontrol::class,'event']);
 
 Route::get('/model',[basecontrol::class,'models']);
 Route::post('/add',[addpostcontrol::class,'add_event']);
