b3244eb2d8b6dd4084656ef32e7293023d5ae120
Kesha005 <backendtm005@gmail.com>
2022-11-24 15:33:14 +0500

    fd

--- app/Http/Controllers/API/basecontrol.php
+++ app/Http/Controllers/API/basecontrol.php
@@ -113,7 +113,7 @@ class basecontrol extends Controller
 
     public function new($new_id)
     {
-        $new_event = newevent::where('id',$new_id)->get();
+        $new_event = newevent::with('product')->where('id',$new_id)->get();
         return response()->json($new_event);
     }
 
