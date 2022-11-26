5ee3023723444b30e7767f3d62cb3cb6acb7a4aa
Kesha005 <backendtm005@gmail.com>
2022-11-26 18:18:17 +0500

    sd

--- app/Http/Controllers/API/basecontrol.php
+++ app/Http/Controllers/API/basecontrol.php
@@ -114,13 +114,13 @@ class basecontrol extends Controller
 
     public function new($new_id)
     {
-        $new_event = newevent::with('product')->where('id', $new_id)->get()->map(function ($item) {
+        $new_event = newevent::with('product','user')->where('id', $new_id)->get()->map(function ($item) {
             if($item->etrap)
             {
                 $welayat=welayat::find($item->etrap->welayat_id); $place=$welayat->name.' '.$item->etrap->name;
             }
-            else {$place='Näbelli ýer';}$user_phone=User::find($item->user_id);
-            return (array)($item->toArray() + ['user_phone'=>$user_phone]+ ['welayat' =>$place]);
+            else {$place='Näbelli ýer';}
+            return (array)($item->toArray() +  ['welayat' =>$place]);
         });;
         return response()->json($new_event);
     }
