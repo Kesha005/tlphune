52abab90735534f855c5946b2ef6a72cfa10f816
Kesha005 <backendtm005@gmail.com>
2022-11-26 18:10:51 +0500

    sd

--- app/Http/Controllers/API/basecontrol.php
+++ app/Http/Controllers/API/basecontrol.php
@@ -119,7 +119,7 @@ class basecontrol extends Controller
                 $welayat=welayat::find($item->etrap->welayat_id); $place=$welayat->name.' '.$item->etrap->name;
             }
             $place='Näbelli ýer';
-            return (array)($item->toArray() +  ['welayat' =>$place]+ ['user_phone'=>$item->user->phone]);
+            return (array)($item->toArray() + ['user_phone'=>$item->user->phone]+ ['welayat' =>$place]);
         });;
         return response()->json($new_event);
     }
