50e0646ceb4f2c4291abe95609dcb6972dedde14
Kesha005 <backendtm005@gmail.com>
2022-11-30 15:23:00 +0500

    sd

--- app/Http/Controllers/API/addpostcontrol.php
+++ app/Http/Controllers/API/addpostcontrol.php
@@ -26,7 +26,7 @@ class addpostcontrol extends Controller
         $data = $request->only('user_id', 'category_id', 'name', 'mark_id', 'place', 'price', 'about');
         $path =  $request->image[0];$filename  = hash('sha256', $path);$image_resize = Image::make($path->getRealPath());
         $image_resize->resize(150, 150);  $image_resize->save(storage_path("/app/public/users/$request->user_id/") . $filename);
-        $data['public_image']="app/public/users/$request->user_id/$filename";$data['is_new']=false;
+        $data['public_image']="/users/$request->user_id/$filename";$data['is_new']=false;
         $event = events::create($data); $this->storeimage($request, $event);
         return response()->json([
             'message' => 'Bildiriş nobata goýuldy admin tassyklandan soň kabul ediler'
