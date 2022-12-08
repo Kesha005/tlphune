94d832fed70124b9723beca4856a35b4b8a8c0b2
Kesha005 <backendtm005@gmail.com>
2022-12-08 15:01:34 +0300

    sd

--- app/Http/Controllers/API/basecontrol.php
+++ app/Http/Controllers/API/basecontrol.php
@@ -99,7 +99,7 @@ class basecontrol extends Controller
            else{$place='Näbelli ýer';} 
             return (array)($item->toArray() + ['user_phone' => $item->user->phone]+ ['welayat' =>$place]);
         });
-        return $events->view;
+        return $events->first()->view;
         return response()->json($events);
     }
 
