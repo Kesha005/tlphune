781fe10d36f169dd132a229cd1de84d5d9992ed4
Kesha005 <backendtm005@gmail.com>
2022-12-22 18:13:07 +0500

    sd

--- resources/views/admin/limit/index.blade.php
+++ resources/views/admin/limit/index.blade.php
@@ -29,19 +29,7 @@ Bildirişlere limit ber
                         </form>
                         @else
 
-                        <form action="{{route('admin.limit.update',$limit->id)}}" id="markform1" enctype="multipart/form-data" method="POST" class="row g-3">
-                            @csrf
-                            @method('PUT')
-                            <div class="col-md-4">
-                                <div class="form-group">
-                                    <label for="name">Günlük bildiriş  sany</label>
-                                    <input type="text" class="form-control" id="name" placeholder="limit bahasy" name="limit" value="{{$limit->limit}}">
-                                </div><br>
-                            </div>
-                            <div class="pull-right">
-                                <button type="submit" class="btn btn-outline-success btn-sm pull-right ">Iber</button>
-                            </div>
-                        </form>
+                       @include('admin.limit.edit')
                         @endif
 
                     </div>
