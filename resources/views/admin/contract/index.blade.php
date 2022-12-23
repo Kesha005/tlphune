4898466da26a3e31dd606642e520e1c48a78b8df
Kesha005 <backendtm005@gmail.com>
2022-12-23 14:00:44 +0300

    s

--- resources/views/admin/contract/index.blade.php
+++ resources/views/admin/contract/index.blade.php
@@ -34,7 +34,7 @@
 
                                         <div class="row">
                                             <div class=" col-md-2 col-lg-1 col-xs-1">
-                                                <form method="post" action="{{route('admin.contract.destroy',contract->id)}}">
+                                                <form method="post" action="{{route('admin.contract.destroy',$contract->id)}}">
                                                     @csrf
                                                     @method('DELETE')
                                                     <a  class="btn btn-outline-info btn-sm" href="{{route('admin.contract.show',$contract->id)}}"><i class="bi bi-eye"></i></a>
