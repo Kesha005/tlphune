f379b5b99ac170c20df2076f27313b5618a8d10c
Kesha005 <backendtm005@gmail.com>
2023-01-03 10:10:23 +0300

    ds

--- app/Http/Controllers/API/addpostcontrol.php
+++ app/Http/Controllers/API/addpostcontrol.php
@@ -7,6 +7,7 @@ use App\Http\Requests\eventrequest;
 use App\Http\Requests\neweventreq;
 use App\Models\event_img;
 use App\Models\events;
+use App\Models\User;
 use App\Models\products;
 use App\Models\welayat;
 use Illuminate\Support\Facades\Storage;
@@ -25,6 +26,7 @@ class addpostcontrol extends Controller
         $image_resize->resize(150, 150);  $image_resize->save(storage_path("/app/public/users/$request->user_id/") . $filename);
         $data['public_image']="/users/$request->user_id/$filename";$data['is_new']=false;
         $event = events::create($data); $this->storeimage($request, $event);
+        $this->user_limit_minus($request->user_id);
         return response()->json([
             'message' => 'Bildiriş nobata goýuldy admin tassyklandan soň kabul ediler'
         ]);
@@ -54,6 +56,7 @@ class addpostcontrol extends Controller
         $data=$request->all();$data['color_id']=$request->color_id;$data['status']=1;$data['name']=$product->name;$data['public_image']=$product->public_image;$data['is_new']=true;
         $data['about']=$product->about;$data['mark_id']=$product->mark_id;$data['category_id']=$product->category_id;
         events::create($data);
+        $this->user_limit_minus($request->user_id);
         return response()->json(['message'=>'Bildiriş nobata goýuldy admin tassyklandan soň kabul ediler']);
     }
 
@@ -61,5 +64,11 @@ class addpostcontrol extends Controller
     {
         $welayats=welayat::with('etrap')->get();
         return response()->json($welayats);
+    }
+
+    public function user_limit_minus($id)
+    {
+        $user=User::find($id);$limit=$user->eventlimit-1;  
+        $user->update(['eventlimit'=>$limit]);  
     }
 }
